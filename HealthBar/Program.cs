4a3ba83a2fb75884e6c56fdc282edb0131e9b99a
Михаил Денисов <migelius.invesy@gmail.com>
2022-05-29 13:53:07 +0300

    Добавьте файлы проекта.

---
+++ HealthBar/Program.cs
@@ -0,0 +1,38 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        float percentHealth = 40f;
+        float maxHealth = 10f;
+        int barPositionX = 0;
+        int barPositionY = 0;
+
+        DrawBar(percentHealth, maxHealth, ConsoleColor.Red, barPositionX, barPositionY);
+    }
+
+    static void DrawBar(float percentageOfBar, float maxBar, ConsoleColor colorPercentBar, int positionX, int positionY)
+    {
+        int maxPercent = 100;
+        float health = maxBar / maxPercent * percentageOfBar;
+        ConsoleColor defaultColor = Console.BackgroundColor;
+        string bar = "";
+
+        for (int i = 0; i < health; i++)
+        {
+            bar += ' ';
+        }
+        Console.SetCursorPosition(positionX, positionY);
+        Console.Write('[');
+        Console.BackgroundColor = colorPercentBar;
+        Console.Write(bar);
+        Console.BackgroundColor = defaultColor;
+
+        bar = "";
+
+        for (var i = health; i < maxBar; i++)
+        {
+            bar += ' ';
+        }
+        Console.Write(bar + ']');
+    }
+}
